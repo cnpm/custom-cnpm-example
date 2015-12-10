85aade7f3145ea1b321714518d5a76a040c87c95
Wang Yitong <wangyitong@souyidai.com>
2015-12-10 22:05:42 +0800

    update cnpmjs.org to 2.5.1 and update config to run

--- cluster/config/index.js
+++ cluster/config/index.js
@@ -5,7 +5,21 @@ fs.existsSync = fs.existsSync || path.existsSync;
 var root = path.dirname(__dirname);
 
 var config = {
-  enableCluster: true
+  enableCluster: true,
+  database: {
+    db: 'cnpmjs_test',
+    username: 'root',
+    password: '',
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    pool: {
+      maxConnections: 10,
+      minConnections: 0,
+      maxIdleTime: 30000
+    },
+    logging: !!process.env.SQL_DEBUG,
+  }
 };
 
 // load config/config.js, everything in config.js will cover the same key in index.js
--- hello_cnpm/config.js
+++ hello_cnpm/config.js
@@ -2,5 +2,19 @@ module.exports = {
   // input your custom config here
   admin: {
     'admin': 'admin@cnpmjs.org'
+  },
+  database: {
+    db: 'cnpmjs_test',
+    username: 'root',
+    password: '',
+    dialect: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    pool: {
+      maxConnections: 10,
+      minConnections: 0,
+      maxIdleTime: 30000
+    },
+    logging: !!process.env.SQL_DEBUG,
   }
 };
